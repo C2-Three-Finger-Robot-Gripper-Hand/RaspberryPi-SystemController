30daa14edb6c6bb206956558673ca0373426969e
Remco Vorthoren <rvorthoren@outlook.com>
2017-12-17 12:29:54 +0100

    add py srcipt

---
+++ modbustest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import minimalmodbus
+import time
+import sys
+
+instrument = minimalmodbus.Instrument('/dev/serial0', 1, minimalmodbus.MODE_ASCII) # port name, slave address (in decimal)
+instrument.serial.baudrate = 115200
+instrument.serial.timeout=0.2
+
+##First calibrate
+
+try:
+	instrument.write_register(3, 2) # Registernumber, calibrating
+	print("Calibrating...")
+except Exception as e:
+  	print(e)
+
+
+answer = input("Proceed: ")
+
+try:
+  instrument.write_register(3, 0) # Registernumber, running
+  print("Running...")
+except Exception as e:
+  print(e)
+
+motor, degree
+while(1):
+  try:
+    motor = input("Motor number: ")
+    degree = input("Enter degrees: ")
+    instrument.write_register(motor - 1, degree) # Registernumber, value
+    print("Writed data: " + str(val))
+  except Exception as e:
+      print(e)
+  time.sleep(1)
+  
+  try:
+    degree = instrument.read_register(motor, 0) # Registernumber, number of decimals
+    print("Current degrees: " + str(degree))
+  except Exception as e:
+    print(e)
+  time.sleep(1)
+
